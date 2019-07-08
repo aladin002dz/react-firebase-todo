39858f27e258e2595d97aae7ae1ce1b1daf752c3
Mahfoudh AROUS <aladin02dz@hotmail.fr>
2019-07-08 13:01:08 +0100

    update

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -8,6 +8,14 @@ class TodosList extends Component {
     this.deleteTodo = this.deleteTodo.bind(this);
   }
 
+  completeTodo = (e, whichTodo, todoName) => {
+    e.preventDefault();
+    firebase.database().ref(`todos/${this.props.userID}/${whichTodo}`).set({
+      todoName: todoName,
+      completed: true
+    });
+  };
+
   deleteTodo = (e, whichTodo) => {
     e.preventDefault();
     const ref = firebase
@@ -22,21 +30,38 @@ class TodosList extends Component {
       return (
         <div className="row" key={item.todoID}>
 
-          <div className="col-8 text-left">
+          <div className="col-3 text-left">
             {item.todoName}
           </div>
-
+          <div className="col-3 text-left">                    
+            {item.completed ? (
+                      <span className="text-success">
+                        completed
+                      </span> 
+                    ) : (
+                      <span className="text-danger">
+                        in progress
+                      </span> 
+                    ) }
+          </div>
           <div
-            className="col-4 text-right"
+            className="col-6 text-right"
             role="group"
             aria-label="Todo Options"
           >
             <button
-              className="btn btn-sm"
+              className="btn btn-sm btn-outline-dark"
+              title="Complete Todo"
+              onClick={e => this.completeTodo(e, item.todoID, item.todoName)}
+            >
+              <i class="fas fa-check"></i> mark as complete
+            </button>
+            <button
+              className="btn btn-sm btn-outline-dark"
               title="Delete Todo"
               onClick={e => this.deleteTodo(e, item.todoID)}
             >
-              <i class="far fa-trash-alt"></i>
+              <i class="far fa-trash-alt"></i> delete
             </button>
           </div>
         </div>
