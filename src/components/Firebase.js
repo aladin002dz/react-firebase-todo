c04a2dbb92021f9c75be449ddc4ad7ad98440267
Mahfoudh AROUS <aladin02dz@hotmail.fr>
2019-07-08 13:44:26 +0100

    update

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -2,18 +2,20 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
 
+const { REACT_APP_API_KEY, REACT_APP_AUTH_DOMAIN, REACT_APP_DATABASE_URL, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET, REACT_APP_MESSAGING_SENDER_ID} = process.env;
 const config = {
-  apiKey: "xxxxxxxxxxxxxxxx",
-  authDomain: "xxxxxxx.firebaseapp.com",
-  databaseURL: "https://xxxxxxxxx.firebaseio.com",
-  projectId: "fir-xxxxxx",
-  storageBucket: "fir-xxxxxxxx.appspot.com",
-  messagingSenderId: "xxxxxxxxxx"
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID
 };
-
 firebase.initializeApp(config);
+
+console.log("firebase40");
+console.log("config"+ JSON.stringify(config));
 
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
-
 export default firebase;
